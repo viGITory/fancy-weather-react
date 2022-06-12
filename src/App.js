99111ad9e3317a68bf86559ca961f42e5a0037ac
PANTORY <pantory@gmail.com>
2022-06-12 15:01:33 +0300

    feat: add func to get weather data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import "./App.css";
+import './App.css';
 
-function App() {
-  return <div className="App"></div>;
-}
+import { useEffect, useState } from 'react';
+
+const App = () => {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [weatherData, setWeatherData] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      navigator.geolocation.getCurrentPosition((pos) => {
+        setLat(pos.coords.latitude);
+        setLong(pos.coords.longitude);
+      });
+
+      await fetch(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=metric&appid=a8122fbe52b443584fbcba6f23095ca1`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setWeatherData(data);
+          console.log(data);
+        });
+    };
+
+    getData();
+  }, [lat, long]);
+
+  return <div className="app"></div>;
+};
 
 export default App;
