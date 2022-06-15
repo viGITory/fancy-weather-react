32bc8274ec46ff79d5d5b693c9fdf3bd7941be0f
PANTORY <pantory@gmail.com>
2022-06-15 22:53:43 +0300

    fix: add weather data check

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -6,13 +6,15 @@ import DailyForecast from '../DailyForecast/DailyForecast';
 import HourlyForecast from '../HourlyForecast/HourlyForecast';
 
 const Weather = ({ weatherData }) => {
-  return (
+  return weatherData.current ? (
     <div className="weather">
       <Clock weatherData={weatherData} />
       <CurrentWeather weatherData={weatherData} />
       <HourlyForecast weatherData={weatherData} />
       <DailyForecast weatherData={weatherData} />
     </div>
+  ) : (
+    <div></div>
   );
 };
 
