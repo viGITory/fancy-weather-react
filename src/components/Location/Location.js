44f27037b38a2af6f9fcde7ea935a3d9be9b81ec
PANTORY <pantory@gmail.com>
2022-07-25 12:42:33 +0300

    fix: country flag display

--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -1,14 +1,23 @@
+import { useEffect, useState } from 'react';
 import './Location.css';
 
 const Location = ({ location }) => {
+  const [flag, setFlag] = useState();
   const { city, country, flagUrl } = location;
+
+  useEffect(() => {
+    const image = new Image();
+    image.src = flagUrl;
+
+    image.addEventListener('load', () => setFlag(image.src));
+  }, [flagUrl]);
 
   return (
     <p className="location">
       {city}, {country.slice(0, -2)}
       <span
         className="location__flag"
-        style={{ backgroundImage: `url(${flagUrl})` }}
+        style={{ backgroundImage: `url(${flag})` }}
       >
         {country.slice(-2)}
       </span>
