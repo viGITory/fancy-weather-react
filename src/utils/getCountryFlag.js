9947ed2c8bd0e5dd342d28665d68c00f5e8eaa31
PANTORY <pantory@gmail.com>
2022-07-30 13:21:05 +0300

    refactor: remove unused utils func

--- src/utils/getCountryFlag.js
+++
@@ -1,9 +0,0 @@
-const getCountryFlag = async (countryCode) => {
-  const url = await fetch(`https://flagcdn.com/${countryCode}.svg`).then(
-    (response) => response.url
-  );
-
-  return url;
-};
-
-export default getCountryFlag;
