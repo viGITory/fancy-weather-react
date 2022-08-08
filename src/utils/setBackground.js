ca2fbbff58b9734951bcd202bae54cbd6d7ec7c6
PANTORY <pantory@gmail.com>
2022-08-08 23:44:03 +0300

    fix: background image number

--- src/utils/setBackground.js
+++ src/utils/setBackground.js
@@ -6,7 +6,8 @@ const setBackground = (imageData) => {
   const filteredImages = imageData.photos.photo.filter(
     (photo) => photo.url_h && photo.width_h === 1600 && photo.height_h === 1067
   );
-  image.src = filteredImages[getRandomInteger(0, filteredImages.length)].url_h;
+  image.src =
+    filteredImages[getRandomInteger(0, filteredImages.length - 1)].url_h;
 
   image.addEventListener(
     'load',
